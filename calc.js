7d4e7ab6ed16654f16c3c197330939008e462a3a
Danijel Gomola <266135542+gomoladanijel@users.noreply.github.com>
2026-03-25 23:34:29 +0100

    Add calc.js

---
+++ calc.js
@@ -0,0 +1,112 @@
+const display = document.querySelector(".display");
+const digits = document.querySelectorAll(".digit");
+const decimal = document.querySelector(".decimal");
+const operators = document.querySelectorAll(".operator");
+const clearBtn = document.querySelector(".clear");
+const equalBtn = document.querySelector(".equal");
+
+let isResultDisplayed = false;
+
+digits.forEach(digit => {
+    digit.addEventListener("click", () => {
+        if (isResultDisplayed) {
+            display.textContent = "";
+            isResultDisplayed = false;
+        }
+
+        display.textContent += digit.textContent;
+    });
+});
+
+decimal.addEventListener("click", () => {
+    const exp = display.textContent.trim().split(" ");
+    const lastChunk = exp[exp.length - 1];
+    const isLastCharOp = ["+", "−", "×", "÷"].includes(lastChunk);
+
+    if (lastChunk.includes(".")) return;
+
+    lastChunk === "" || isLastCharOp
+        ? display.textContent += "0." 
+        : display.textContent += decimal.textContent;
+})
+
+operators.forEach(operator => {
+    operator.addEventListener("click", () => {
+        isResultDisplayed = false;
+        const exp = display.textContent.trim().split(" ");
+        const isLastCharOp = ["+", "−", "×", "÷"].includes(display.textContent.trim().slice(-1));
+
+        if (exp[0] === "×" || exp[0] === "÷" || isLastCharOp) {
+            display.style.fontSize = "24px";
+            display.style.color = "#ff2600";
+            display.textContent = "Malformed expression";
+            disableButtons();
+            return;
+        }
+
+        if (exp[0] === "+" || exp[0] === "−") operate();
+
+        if (exp[1] === "÷" && exp[2] === "0") operate();
+
+        if (exp.length === 3) {
+            operate();
+            display.textContent += " " + operator.textContent + " ";
+        } else {
+            display.textContent += " " + operator.textContent + " ";
+        }
+    });
+});
+
+clearBtn.addEventListener("click", () => {
+    digits.forEach(digit => digit.disabled = false);
+    decimal.disabled = false;
+    operators.forEach(operator => operator.disabled = false);
+
+    display.style.color = "#fff";
+    display.style.fontSize = "32px";
+    display.textContent = "";
+});
+
+equalBtn.addEventListener("click", operate);
+
+function operate() {
+    const nums = display.textContent.split(/[÷×−+]/);
+    const n1 = +nums[0];
+    const n2 = +nums[1];
+    const op = display.textContent.match(/[÷×−+]/).toString();
+
+    let result;
+
+    switch (op) {
+        case "+":
+            result = n1 + n2;
+            break;
+        case "−":
+            result = n1 - n2;
+            break;
+        case "×":
+            result = n1 * n2;
+            break;
+        case "÷":
+            if (n2 === 0) {
+                display.style.fontSize = "24px";
+                display.style.color = "#ff9500";
+                display.textContent = "Cannot divide by zero";
+                disableButtons();
+                return;
+            } else {
+                result = n1 / n2;
+                break;
+            }
+        default:
+            break;
+    }
+    
+    display.textContent = Math.round(result * 100) / 100;
+    isResultDisplayed = true;
+}
+
+function disableButtons() {
+    digits.forEach(digit => digit.disabled = true);
+    operators.forEach(operator => operator.disabled = true);
+}
